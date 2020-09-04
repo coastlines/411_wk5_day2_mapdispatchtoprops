d5baa48d41e1935a37fa4929f2b55ca0b2159ab4
efaulkenberry@gmail.com <efaulkenberry@gmail.com>
2020-09-03 19:58:38 -0500

    addCar

---
+++ src/containers/AddCar.js
@@ -0,0 +1,15 @@
+import { connect } from 'react-redux'
+import AddCar from '../components/AddCar'
+import { addCar } from '../redux/actions'
+
+// mapDispatchToProps
+// takes one argument called "dispatch"
+// returns an object who's keys are the props you want to pass. 
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+      addCar: (car) => dispatch(addCar(car))
+  }
+}
+
+export default connect(null, mapDispatchToProps)(AddCar)
---
+++ src/redux/actions.js
@@ -0,0 +1,13 @@
+const addCar = (car) => {
+  return {
+      type: 'ADD_CAR',
+      value: car
+  }
+}
+
+const removeCar = (index) => {
+  return {
+      type: 'REMOVE_CAR',
+      value: index
+  }
+}
